eae6e09d801b8cc1b2edeac675b20f45839cc3e3
tramyu <tramyu@naver.com>
2019-04-14 17:05:01 +0900

    LoginUser 어노테이션 / ArgumentResolver 구현 #4

---
+++ skeleton-api/src/main/java/com/kyunam/skeleton/common/annotation/LoginUser.kt
@@ -0,0 +1,8 @@
+package com.kyunam.skeleton.common.annotation
+
+import java.lang.annotation.Documented
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@Documented
+annotation class LoginUser
